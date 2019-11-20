fdb074a8199d1bdf032ede756aed64f7bdff03e3
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 15:18:08 +0100

    calculator

---
+++ calculator.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* inputs();
+
+int main(void){
+	for (;;){
+		printf("~~ Medusa calculator ~~\n");
+		printf("1. Addition\n");
+		printf("2. Subtraction\n");
+		printf("3. Multiplication\n");
+		printf("4. Division\n");
+		printf("Enter choice\n>> ");
+		
+		int choice;
+		if (!scanf("%d", &choice)){
+			printf("Invalid input.\n");
+			return 0;
+		}
+
+		int* values = inputs();
+		switch (choice){
+			case 1:
+				printf("Answer: %d\n", values[0]+values[1]);
+				break;
+			case 2:
+				printf("Answer: %d\n", values[0]-values[1]);
+				break;
+			case 3:
+				printf("Answer: %d\n", values[0]*values[1]);
+				break;		
+			case 4:
+				printf("Answer: %d\n", values[0]/values[1]);
+				break;
+			default:
+				printf("Invalid input\n");
+		}
+	}
+	return 0;
+}
+
+int* inputs(){
+	int* numbers = malloc(sizeof(int)*2);
+	printf("Enter number 1\n>> ");
+	scanf("%d", &numbers[0]);
+	printf("Enter number 2\n>> ");
+	scanf("%d", &numbers[1]);
+	return numbers;
+}
