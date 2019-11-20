6813b523ec52f390090f84bf564e16b4116ca945
debattista006 <56856740+debattista006@users.noreply.github.com>
2019-11-20 15:21:05 +0100

    Added option 5 in menu and configured exit for option 5.

--- calculator.c
+++ calculator.c
@@ -10,6 +10,7 @@ int main(void){
 		printf("2. Subtraction\n");
 		printf("3. Multiplication\n");
 		printf("4. Division\n");
+        printf("5. Exit\n");
 		printf("Enter choice\n>> ");
 		
 		int choice;
@@ -17,6 +18,10 @@ int main(void){
 			printf("Invalid input.\n");
 			return 0;
 		}
+            
+        if (choice == 5);
+            return 0;
+    
 
 		int* values = inputs();
 		switch (choice){
